3ec8128779d3b8106b2c1de01c849bcb51733903
S. Everett Abbott <severettabbott@gmail.com>
2017-11-15 13:06:27 -0800

    made palette dynamically generated and introduced color changing functionality.

--- index.js
+++ index.js
@@ -1,61 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let palette = document.querySelector('.palette');
   let currentColor = '';
-  let pWidth = 50;
-  let pHeight = 50;
-  let cWidth = 400;
-  let cHeight = 400;
+  let sizes = {
+    pWidth: 50,
+    pHeight: 50,
+    cWidth: 500,
+    cHeight: 500,
+    sWidth: 50,
+    sHeight: 50,
+    sMargin: 10
+  };
+  let colorsList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white', 'magenta'];
 
-  // Set the pixel color
-  /*canvas.addEventListener('click', (e) => {
-    let pixel = e.target;
-    let pixelColor = pixel.classList[1];
-    if (!pixelColor) {
-      pixel.classList.add(currentColor);
-    } else if (pixelColor !== currentColor) {
-      pixel.classList.remove(pixelColor);
-      pixel.classList.add(currentColor);
-    } else if (pixelColor === currentColor) {
-      pixel.classList.remove(pixelColor);
-    }
-  });*/
+  render(sizes, colorsList);
+  let canvas = document.querySelector('.canvas');
+  let palette = document.querySelector('.palette');
 
   // Select the Color from the palette
   palette.addEventListener('click', (e) => {
     let selectedColor = e.target;
     if (selectedColor.id !== 'selected') {
-      var oldColor = document.getElementById('selected');
+      let oldColor = document.getElementById('selected');
       if (oldColor) {
         oldColor.setAttribute('id', '');
       }
       selectedColor.setAttribute('id', 'selected');
-      currentColor = selectedColor.classList[0];
+      currentColor = selectedColor.style.backgroundColor;
     }
   });
 
-  render(pWidth, pHeight, cWidth, cHeight);
+  // Set the pixel color
+  canvas.addEventListener('click', (e) => {
+    let pixel = e.target;
+    let pixelColor = pixel.style.backgroundColor;
+    if (pixelColor === currentColor) {
+      pixel.style.backgroundColor = 'white';
+    } else {
+      pixel.style.backgroundColor = currentColor;
+    }
+  });
+
 });
 
-function render (pWidth, pHeight, cWidth, cHeight) {
+function render (sizes, colorsList) {
   let canvas = document.createElement('div');
-  canvas.style.width = cWidth + 'px';
-  canvas.style.height = cHeight + 'px';
-  canvas.style.fontSize = 0;
-  canvas.style.outline = '2px solid black'
-  document.body.prepend(canvas);
+  canvas.style.width = sizes.cWidth + 'px';
+  canvas.style.height = sizes.cHeight + 'px';
+  canvas.classList.add('canvas');
+  document.body.append(canvas);
 
-  var renderPixel = function (width, height) {
+  let palette = document.createElement('div');
+  palette.style.width = ((sizes.sHeight + sizes.sMargin * 2) * colorsList.length) + 'px';
+  palette.style.height = (sizes.sHeight + sizes.sMargin * 2) + 'px';
+  palette.classList.add('palette');
+  document.body.append(palette);
+
+  let renderPaletteSwatch = function(color) {
+      let swatch = document.createElement('div');
+      swatch.style.width = sizes.sWidth + 'px';
+      swatch.style.height = sizes.sHeight + 'px';
+      swatch.style.margin = sizes.sMargin + 'px';
+      swatch.style.backgroundColor = color;
+      swatch.classList.add('swatch');
+      palette.append(swatch);
+  };
+
+  colorsList.forEach((element) => {
+    renderPaletteSwatch(element);
+  });
+
+  let renderPixel = function (width, height) {
     let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
     pixel.style.width = width + 'px';
     pixel.style.height = height + 'px';
-    pixel.style.display = 'inline-block';
-    pixel.style.outline = '1px dashed grey';
+    pixel.style.backgroundColor = 'white';
     canvas.append(pixel);
   };
 
-  for (let h = 0; h < cHeight; h += pHeight) {
-    for (let w = 0; w < cWidth; w += pWidth) {
-      renderPixel(pWidth, pHeight);
+  for (let h = 0; h < sizes.cHeight; h += sizes.pHeight) {
+    for (let w = 0; w < sizes.cWidth; w += sizes.pWidth) {
+      renderPixel(sizes.pWidth, sizes.pHeight);
     }
   }
 
